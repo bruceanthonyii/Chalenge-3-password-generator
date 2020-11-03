b0797ee801151fafa3e3907be7c28e301e29b3d3
Brock <bruceanthonyii@gmail.com>
2020-11-02 23:10:06 -0600

    added passlength prompt

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
 // Assignment code here
 
+var generatePassword = function() {
+
+  window.alert("Please provide password criteria");
+
+  window.alert("Please choose a password length.");
+
+  var passLength = function() {
+
+    var lengthNumber = window.prompt("Please enter a number between 8 and 128");
+
+    if (lengthNumber === "" || lengthNumber === null || lengthNumber <= 7 || lengthNumber >=129 ) {
+      window.alert("Please enter a valid number");
+      return passLength();
+    }
+
+  }
+
+  passLength();
+
+  window.alert("Please choose character type(s)");
+
+  var passChar = function() {
+
+    var charType = window.prompt("Choose type(s): 1 for lowercase, 2 for uppercase, 3 for numeric, and/or 4 for special characters");
+
+    
+      
+        
+      
+  
+  }
+
+
+}
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,3 +50,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
